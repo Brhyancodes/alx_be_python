71a642d44488d5383511617b4d513c982ee86d34
Brian Wakhale <brianweke1998@gmail.com>
2025-07-19 18:20:20 +0300

    Completed Multiplication table task

---
+++ control-flow/multiplication_table.py
@@ -0,0 +1,7 @@
+# For loop for multiplication table
+number = int(input("Enter a number to see its multiplication table: "))
+
+# Generate and Print the Multiplication Table:
+for i in range(1, 11):
+    result = number * i
+    print(f"{number} * {i} = {result}")
