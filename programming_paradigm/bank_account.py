508283a3b28c903978bcee1f0716a229bcfb2921
Brian Wakhale <brianweke1998@gmail.com>
2025-08-01 11:13:03 +0300

    Completed task 1 for bank account

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,31 @@
+class BankAccount:
+    def __init__(self, initial_balance=0):
+        self.balance = initial_balance
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.balance += amount
+            print(f"Deposited: ${amount:.2f}")
+        else:
+            print("Deposit amount must be positive.")
+
+    def withdraw(self, amount):
+        if 0 < amount <= self.balance:
+            self.balance -= amount
+            print(f"Withdrew: ${amount:.2f}")
+            return True
+        else:
+            print(
+                "Withdrawal amount must be positive and less than or equal to the balance."
+            )
+            return False
+
+    def display_balance(self):
+        print(f"Current balance: ${self.balance:.2f}")
+
+
+if __name__ == "__main__":
+    account = BankAccount(10000)
+    account.deposit(50000)
+    account.withdraw(30000)
+    account.display_balance()
