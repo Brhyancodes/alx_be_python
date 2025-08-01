94bba093c75e53ac11e167b444bbc398560df858
Brian Wakhale <brianweke1998@gmail.com>
2025-08-01 11:21:36 +0300

    Modified deposit error to 1 decimal point

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -5,14 +5,14 @@ class BankAccount:
     def deposit(self, amount):
         if amount > 0:
             self.balance += amount
-            print(f"Deposited: ${amount:.2f}")
+            print(f"Deposited: ${amount:.1f}")
         else:
             print("Deposit amount must be positive.")
 
     def withdraw(self, amount):
         if 0 < amount <= self.balance:
             self.balance -= amount
-            print(f"Withdrew: ${amount:.2f}")
+            print(f"Withdrew: ${amount:.1f}")
             return True
         else:
             print(
@@ -21,9 +21,10 @@ class BankAccount:
             return False
 
     def display_balance(self):
-        print(f"Current Balance: ${self.balance:.2f}")
+        print(f"Current Balance: ${self.balance:.1f}")
 
 
+# Optional: Only runs when executing this file directly
 if __name__ == "__main__":
     account = BankAccount(10000)
     account.deposit(50000)
